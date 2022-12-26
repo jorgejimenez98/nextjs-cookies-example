03cd8d984d878619eb1224cff9d3d285467b6a98
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-26 04:40:04 +0100

    add theme design

--- pages/theme-changer/index.tsx
+++ pages/theme-changer/index.tsx
@@ -1,9 +1,30 @@
-import { FC } from 'react'
+import { CardContent, FormControl, FormLabel, Card, RadioGroup, FormControlLabel, Radio } from '@mui/material'
+import { ChangeEvent, FC, useState } from 'react'
 import { Layout } from '../../components/layouts'
 
 const ThemeChangerPage: FC = () =>{
+  const [currentTheme, setCurrentTheme] = useState('ligth')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurrentTheme(e.target.value)
+  }
+
   return <Layout>
-    <h1>ThemeChangerPage Master</h1>
+    <Card>
+      <CardContent>
+        <FormControl>
+          <FormLabel>Tema</FormLabel>
+          <RadioGroup
+            value={currentTheme}
+            onChange={handleChange}
+          >
+            <FormControlLabel value={'ligth'} control={<Radio />} label='ligth' />
+            <FormControlLabel value={'dark'} control={<Radio />} label='dark' />
+            <FormControlLabel value={'custom'} control={<Radio />} label='custom' />
+          </RadioGroup>
+        </FormControl>
+      </CardContent>
+    </Card>
   </Layout>
 }
 
