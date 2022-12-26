7ea3bb9d8197418aa7cae0ffbce5dae492337bdb
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-26 04:16:31 +0100

    add mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+
+
 import '../styles/globals.css'
+import { darkTheme } from '../themes'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
